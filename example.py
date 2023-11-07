3bf4cc398ce273e1209f650d0f466a1a2be9265c
keregurs <kerem@gursu.org>
2023-11-07 18:49:11 +0300

    correct credentials on login file

--- example.py
+++ example.py
@@ -8,22 +8,10 @@ from util.api_eox import ApiEox
 
 
 def main():
-    """Example usage of Cisco Support API utilities
 
-    This example requires a Cisco Support API client key and secret
-    stored in a .env file in the current directory. These credentials
-    are used to login to the Cisco Support API to obtain an authentication
-    token, which is then used with the EoX API end-point. Two example
-    Cisco PID's are used to show the use of the EoX API and the results
-    are saved to a CSV file.
-    """
-
-    client_key = dotenv_values('.env')['CLIENT_KEY']
-    client_secret = dotenv_values('.env')['CLIENT_SECRET']
-
-    api = ApiLogin(client_key, client_secret)
+    api = ApiLogin('h9mrtrht6kvckq7cuvh2qqzj', '9vhW3wgHMM5mC56xSexAneut')
     eox = ApiEox(api.auth_token)
-    pids = ['WS-C3750X-48PF-S', 'C3KX-PWR-1100WAC', ]
+    pids = ['LIC-VVCS-C-M-PAK', 'L-ISE-BSE-P5', 'M-ASR1002X-8GB', ]
     eox.query_by_pid(pids)
 
     FNAME = 'eox_report.csv'
@@ -48,7 +36,7 @@ def main():
                              record['EndOfSWMaintenanceReleases']['value'],
                              record['EOXExternalAnnouncementDate']['value'],
                              record['EndOfSaleDate']['value'],
-                             record['EndOfSecurityVulSupportDate']['value'],
+                             #record['EndOfSecurityVulSupportDate']['value'],
                              record['EndOfRoutineFailureAnalysisDate']['value'],
                              record['EndOfServiceContractRenewal']['value'],
                              record['EndOfSvcAttachDate']['value'],
